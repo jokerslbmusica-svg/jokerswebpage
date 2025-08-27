3d8ae385a6d82750bc712497eeae39f5b4060d09
jokerslbmusica-svg <jokerslbmusica@gmail.com>
2025-08-27 15:48:37 -0600

    Delete src/app/actions/band-info.actions.ts

--- src/app/actions/band-info.actions.ts
+++
@@ -1,5 +0,0 @@
-// This file is obsolete and its content has been moved to src/app/actions.ts.
-// It is kept to prevent breaking existing imports, but it should not be used for new development.
-// The build process might fail if this file is deleted directly.
-'use server';
-export * from '../actions';
