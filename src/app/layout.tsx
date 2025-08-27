728a5c6c68f6f9e495a8791c8ac8636574427607
jokerslbmusica-svg <jokerslbmusica@gmail.com>
2025-08-27 16:07:51 -0600

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import { AuthProvider } from '@/hooks/use-auth';
 import dynamic from 'next/dynamic';
 
 const AuthProviderWithNoSSR = dynamic(
