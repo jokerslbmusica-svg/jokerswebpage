eba39f443486d12b1135ec15a459fd8a2252f899
jokerslbmusica-svg <jokerslbmusica@gmail.com>
2025-08-27 09:43:11 -0600

    dev.ts

--- src/ia/dev.ts
+++ src/ia/dev.ts
@@ -1,7 +1,1 @@
-import { config } from 'dotenv';
-config();
 
-import '@/ai/flows/suggest-hashtags.ts';
-import '@/ai/flows/generate-bio.ts';
-import '@/ai/flows/generate-social-post.ts';
-import '@/ai/flows/generate-logo.ts';
