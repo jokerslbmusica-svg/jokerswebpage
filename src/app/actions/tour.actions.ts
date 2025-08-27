1b4a07ce0b5cabceddce9980b4d1cbb9fa0e7548
jokerslbmusica-svg <jokerslbmusica@gmail.com>
2025-08-27 17:19:19 -0600

    Update tour.actions.ts

--- src/app/actions/tour.actions.ts
+++ src/app/actions/tour.actions.ts
@@ -1,3 +1,1 @@
-// This file is obsolete and its content has been moved to src/app/actions.ts.
-// It is kept to prevent breaking existing imports, but it should not be used for new development.
-// The build process might fail if this file is deleted directly.
+
