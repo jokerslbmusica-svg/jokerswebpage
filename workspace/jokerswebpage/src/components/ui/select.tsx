c2fe9384c1c312f7a25f169c157a72bae14e8490
jokerslbmusica-svg <jokerslbmusica@gmail.com>
2025-09-08 19:59:29 +0000

    Style: Ajuste final de la configuración del proyecto y estilos

--- workspace/jokerswebpage/src/components/ui/select.tsx
+++ workspace/jokerswebpage/src/components/ui/select.tsx
@@ -1,8 +1,6 @@
 "use client"
 
 import * as React from "react"
-import * as React from "react"ct"
-import * as ReactDOM from "react"
 import * as SelectPrimitive from "@radix-ui/react-select"
 import { Check, ChevronDown, ChevronUp } from "lucide-react"
 
